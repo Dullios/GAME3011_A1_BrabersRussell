a3709a44ea589dec7b99846203f3eb7759daf261
Russell Brabers <brabers.russell@gmail.com>
2021-01-17 01:16:26 -0500

    Player setup
    Established PlayerController and Enum file

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    [SerializeField]
+    private GameState gameState;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        gameState = GameState.MainGame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(gameState)
+        {
+            case GameState.MainGame:
+                Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), transform.position.y, Input.GetAxis("Vertical"));
+
+                movement *= speed;
+                rigidBody.velocity = movement;
+                break;
+            
+            case GameState.MiniGame:
+
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/SpotSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotSpawner : MonoBehaviour
+{
+    private SpotSpawner instance;
+
+    [Header("Spawn Details")]
+    public GameObject spotPrefab;
+    public float minX, maxX;
+    public float minY, maxY;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
